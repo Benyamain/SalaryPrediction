1de875a1b546defbb86556f3367008400224168a
Benyamain <yacoobbenyamain@gmail.com>
2023-05-12 03:00:08 -0400

    Everything is going well with setting up the website! Wooooo

--- predict_page.py
+++ predict_page.py
@@ -16,3 +16,32 @@ le_education = data['le_education']
 def show_predict_page():
     st.title('Software Developer Salary Prediction')
     st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        'United States of America',
+        'Germany',
+        'United Kingdom of Great Britain and Northern Ireland',
+        'India',
+        'Canada',
+        'France',
+        'Brazil',
+        'Spain',
+        'Netherlands',
+        'Australia',
+        'Italy',
+        'Poland',
+        'Sweden',
+        'Russian Federatio',
+        'Switzerland'
+    )
+
+    education = (
+        'Less than a Bachelors',
+        'Bachelor’s degree',
+        'Master’s degree',
+        'Post grad',
+    )
+
+    country = st.selectbox('Country', countries)
+    education = st.selectbox('Education Level', education)
+    experience = st.slider('Years of Experience', 0, 50, 3)
